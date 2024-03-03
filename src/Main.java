10ce15e93a116fdfb40013789ac91b399c3b3797
CarEsteban <car23016@uvg.edu.gt>
2024-03-02 19:17:01 -0600

    example of analyzer working

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        // Ejemplo de uso
-        Analyzer.evaluar("(3 4 +)");
+    public static void main(String[] args) {        
+        Analyzer analyzer = new Analyzer();
+        //ArrayList<String> result = analyzer.analyze("3 4 2 + *");
+        ArrayList<String> result = analyzer.analyze("(quote 'hola')");
+        System.out.println(result);
+
     }
 }
