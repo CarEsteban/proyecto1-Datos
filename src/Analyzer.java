5c4a9611505922836aad43f5a3efce74d812cd72
CarEsteban <car23016@uvg.edu.gt>
2024-03-04 23:16:16 -0600

    improvements to regular arithmetic expression syntax

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,18 +5,15 @@ public class Analyzer {
     // Método para analizar el string de entrada
     public int tokenizer(String input) {
 		//tokens para operaciones aritmeticas 
-		if (analyzer("^[(][ ]+((([0-9]+[ ]))|(([+-/]+[ ]))|(([(]+[ ]))|(([)]+[ ])))+[)][ ]$",input)) //para operaciones aritmeticas
+		if (analyzer("^[(][ ]+([+-/*]+[ ])((([0-9]+[ ]))|(([+-/*]+[ ]))|(([(]+[ ]))|(([)]+[ ])))+[)][ ]*$",input)) //para operaciones aritmeticas
 			return 1;
         //tokens para quotes
-        else if (analyzer("^[(][ ]+(quote+[ ])((.))+[)][ ]$",input)) //para operaciones aritmeticas
+        else if (analyzer("^[(][ ]+(quote+[ ])((.))+[)][ ]*$",input)) //para operaciones aritmeticas
             return 2;
-        //tokens para quotes
-        else if (analyzer("^[(][ ]+(setq+[ ])+([a-z]+[ ])((.))+[)][ ]$",input)) //para operaciones aritmeticas
+        //tokens para setq
+        else if (analyzer("^[(][ ]+(setq+[ ])+([a-z]+[ ])((.))+[)][ ]*$",input)) //para operaciones aritmeticas
             return 3;
         
-        
-		//else if (analyzer("^[(][ ][-][ ]+([a-z]+|[0-9]+)[ ]+([a-z]+|[0-9]+)[ ][)]$",input)) //This is a simple add operation of 2 operands
-		//	return 2;
 		else 
 			return -1; 
     }
