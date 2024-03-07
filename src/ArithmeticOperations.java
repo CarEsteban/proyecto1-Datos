5947b96d37e32ef7a4d1cf6bd8e3af290a1bf5bf
MiltonPolanco <pol2347@uvg.edu.gt>
2024-03-07 11:34:10 -0600

    ArithmeticOperations v2 y pruebas unitarias de ArithmeticOperations v2

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -1,45 +1,43 @@
 import java.util.Scanner;
 
 public class ArithmeticOperations implements IFunction {
-    public void processOperation(String operation, String input) {
-        if (operation.equals("arithmetic")) {
-            // Elimina los paréntesis y divide la expresión en tokens
-            String[] tokens = input.replaceAll("[()]", "").split("\\s+");
+    public String processOperation(String input) {
+        // Elimina los paréntesis y divide la expresión en tokens
+        String[] tokens = input.replaceAll("[()]", "").split("\\s+");
 
-            if (tokens.length >= 3) {
-                // El primer token es el operador (+, -, *, /)
-                String operator = tokens[0];
+        if (tokens.length >= 3) {
+            // El primer token es el operador (+, -, *, /)
+            String operator = tokens[0];
 
-                // Los siguientes tokens son operandos
-                double operand1 = Double.parseDouble(tokens[1]);
-                double operand2 = Double.parseDouble(tokens[2]);
+            // Los siguientes tokens son operandos
+            double operand1 = Double.parseDouble(tokens[1]);
+            double operand2 = Double.parseDouble(tokens[2]);
 
-                // Realiza la operación aritmética
-                double result = 0;
-                switch (operator) {
-                    case "+":
-                        result = operand1 + operand2;
-                        break;
-                    case "-":
-                        result = operand1 - operand2;
-                        break;
-                    case "*":
-                        result = operand1 * operand2;
-                        break;
-                    case "/":
+            // Realiza la operación aritmética
+            double result = 0;
+            switch (operator) {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                case "/":
+                    if (operand2 != 0) {
                         result = operand1 / operand2;
-                        break;
-                    default:
-                        System.out.println("Operador no válido");
-                        return;
-                }
-
-                System.out.println("Resultado de la operación: " + result);
-            } else {
-                System.out.println("Error en la expresión aritmética");
+                    } else {
+                        return "Error: División por cero";
+                    }
+                    break;
+                default:
+                    return "Operador no válido";
             }
+            return "Resultado de la operación: " + result;
         } else {
-            System.out.println("Operación no soportada por ArithmeticOperations");
+            return "Error en la expresión aritmética";
         }
     }
 }
