75b61b0f914ba78cfec8da8f90529eee91b45f03
Isabella0334 <isabellarecinos@gmail.com>
2024-03-07 10:25:26 -0600

    Base para clase Enviroment

--- src/Environment.java
+++ src/Environment.java
@@ -1,3 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Environment {
-    
+    private Map<String, Object> variables;
+
+    public Environment() {
+        this.variables = new HashMap<>();
+    }
+
+    public void defineVariable(String name, Object value) {
+        variables.put(name, value);
+    }
+
+    public Object getVariable(String name) {
+        return variables.get(name);
+    }
+
+    public boolean variableExists(String name) {
+        return variables.containsKey(name);
+    }
+
+    public void removeVariable(String name) {
+        variables.remove(name);
+    }
 }
+
