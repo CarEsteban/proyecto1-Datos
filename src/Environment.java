40de7cf49fce2d3d2e6614cfd11702032a825db9
Isabella0334 <isabellarecinos@gmail.com>
2024-03-07 11:34:26 -0600

    Environment modificado

--- src/Environment.java
+++ src/Environment.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class Environment {
     private Map<String, Object> variables;
-    private Environment parent; // Si el entorno tiene un entorno padre
+    private Environment parent;
 
     public Environment() {
         this.variables = new HashMap<>();
@@ -22,21 +22,20 @@ public class Environment {
     public Object getVariable(String name) {
         Object value = variables.get(name);
         if (value != null) {
-            return value; // La variable está definida en este entorno
+            return value;
         } else if (parent != null) {
-            return parent.getVariable(name); // Busca en el entorno padre
+            return parent.getVariable(name);
         } else {
-            return null; // La variable no está definida en ningún entorno
+            return null;
         }
     }
 
     public void setVariable(String name, Object value) {
         if (variables.containsKey(name)) {
-            variables.put(name, value); // Actualiza el valor de la variable existente
+            variables.put(name, value);
         } else if (parent != null) {
-            parent.setVariable(name, value); // Si no está en este entorno, busca en el padre
+            parent.setVariable(name, value);
         } else {
-            // La variable no está definida en ningún entorno, puedes definirla aquí
             defineVariable(name, value);
         }
     }
