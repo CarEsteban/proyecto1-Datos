da1655946c6dc2fa2eabe2200484f9008da483e9
CarEsteban <car23016@uvg.edu.gt>
2024-03-10 14:23:10 -0600

    cond semi-functional

--- src/Cond.java
+++ src/Cond.java
@@ -1,19 +1,60 @@
 import java.util.Arrays;
 
+import javax.print.DocFlavor.READER;
+
 public class Cond implements IFunction  {
     //para que funcione el cond, he imprimir en pantalla se usa quote
-    String operator, n1, n2;
+    String operator, resp1,resp2;
+    int n1, n2;
     @Override
     public String execute(String input, Environment env) {
         // reducir el input de entrada
         String[] tokens = input.trim().replaceAll("[()]", "").trim().split("\\s+");
         operator = tokens[1];
-        n1 = tokens[2];
-        n2 = tokens[3];
-        if (tokens.length >= 3) {
+        n1 = Integer.parseInt(tokens[2]);
+        n2 = Integer.parseInt(tokens[3]);
+        resp1 = tokens[5];
+        resp2 = tokens[7];
 
-            //Imprimir el array de tokens para depuración
-            return Arrays.toString(tokens);
+        
+        if (tokens.length >= 3 & tokens.length ==8) {
+
+            switch (operator) {
+                case "<":
+                    if (n1<n2) {
+                        return resp1;
+                    }else{
+                        return resp2;
+                    }
+                case "<=":
+                    if(n1<=n2){
+                        return resp1;
+                    }else{
+                        return resp2;
+                    }
+                case ">":
+                    if(n1>n2){
+                        return resp1;
+                    }else{
+                        return resp2;
+                    }
+                case ">=":
+                    if(n1>=n2){
+                        return resp1;
+                    }else{
+                        return resp2;
+                    }
+                case "==":
+                    if(n1==n2){
+                        return resp1;
+                    }else{
+                        return resp2;
+                    }
+                case "equal":
+                    return "me falta implementar el equal we";
+                default:
+                    return "no joto";
+            }
             
         } else {
             return "Error: input no válido.";
