cffbcf7333c7b742b1494d74cfc0ef2a97c803a9
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2024-03-11 19:48:24 -0600

    Update Quote.java

--- src/Quote.java
+++ src/Quote.java
@@ -1,8 +1,17 @@
-public class Quote implements IFunction{
-
+public class Quote implements IFunction {
     @Override
-    public String execute(String input, Environment env) {
-        return "si funciona quote";
+    public String processOperation(String expression) {
+        // Mostrar la expresión
+        System.out.println("Expresión original: " + expression);
+
+        // Pasar string y borrar la primera parte
+        String[] parts = expression.split("\\s+", 2);
+        if (parts.length < 2) {
+            return "Invalid quote expression";
+        }
+        String remaining = parts[1];
+
+        // Retornar la frase
+        return remaining.trim();
     }
-    
 }
