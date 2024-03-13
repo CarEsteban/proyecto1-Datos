36965eb060ae6356b1632773ec036637f2e84b39
CarEsteban <car23016@uvg.edu.gt>
2024-03-12 22:21:59 -0600

    fixing quote

--- src/Quote.java
+++ src/Quote.java
@@ -1,7 +1,8 @@
 public class Quote implements IFunction {
 
     public String execute(String expression,Environment env) {
-        String expressionWithoutQuote = expression.substring(expression.indexOf(" ") + 1);
+        String expressionWithoutQuote = expression.trim().substring(1, expression.trim().length() - 1).trim();
+        
         if (expressionWithoutQuote.startsWith("\"") && expressionWithoutQuote.endsWith("\"")) {
             return expressionWithoutQuote.substring(1, expressionWithoutQuote.length() - 1);
         } else {
