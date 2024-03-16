42dda1f9f6c274944bbb894d89e1889adec9686f
CarEsteban <car23016@uvg.edu.gt>
2024-03-16 13:57:21 -0600

    quote fixed and functional

--- src/Quote.java
+++ src/Quote.java
@@ -1,23 +1,20 @@
 public class Quote implements IFunction {
 
-    private String expression;
-
-    public Quote(String expression) {
-        this.expression = expression;
-    }
-
     @Override
     public String execute(String input, Environment env) {
         String[] tokens = input.trim().replaceAll("[()]", "").trim().split("\\s+");
+
         if (tokens.length == 2) {
             String quoted = tokens[1];
-            if (quoted.startsWith("\"") && quoted.endsWith("\"")) {
-                return quoted.substring(1, quoted.length() - 1);
-            } else {
-                return "sintax error";
+
+            if(env.variableExists(quoted)){
+                return env.getStringVariable(quoted);
+            }else{
+                return quoted;
             }
+
         } else {
-            return "Error: input no válido para Quote.";
+            return "Index out of bounds exception";
         }
     }
 }
