f669ceb16a02341bc84d7b9947f24759821cef74
Isabella0334 <isabellarecinos@gmail.com>
2024-03-18 11:24:37 -0600

    Functional predicate tests with Variable Management

--- test/PredicateTest.java
+++ test/PredicateTest.java
@@ -1,73 +1,66 @@
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class PredicateTest {
 
-    @Test
-    public void testAtom() {
-        Environment env = new Environment();
-        Predicate predicate = new Predicate();
+    private Predicate predicate;
+    private Environment environment;
 
-        // Prueba con un número (átomo)
-        assertEquals("true", predicate.execute("(atom 42)", env));
-
-        // Prueba con una cadena (átomo)
-        assertEquals("true", predicate.execute("(atom foo)", env));
-
-        // Prueba con una lista (no debería ser un átomo)
-        assertEquals("false", predicate.execute("(atom (1 2 3))", env));
+    @Before
+    public void setUp() {
+        predicate = new Predicate();
+        environment = new Environment();
+        // Configurar el entorno con algunas variables de prueba
+        environment.defineVariable("num1", "10");
+        environment.defineVariable("num2", "20");
+        environment.defineVariable("str1", "test");
+        environment.defineVariable("list1", "(a b c)");
     }
 
     @Test
-    public void testList() {
-        Environment env = new Environment();
-        Predicate predicate = new Predicate();
+    public void testAtomWithLiteral() {
+        assertEquals("true", predicate.execute("(atom a)", environment));
+        assertEquals("true", predicate.execute("(atom 123)", environment));
 
-        // Prueba con una lista
-        env.defineVariable("list", new ArrayList<>(List.of(1, 2, 3))); // Asegurando que sea aceptado como una lista.
-        assertEquals("true", predicate.execute("(list (list 1 2 3))", env));
-
-        // Prueba con un átomo (no debería ser una lista)
-        assertEquals("false", predicate.execute("(list foo)", env));
     }
 
     @Test
-    public void testEqual() {
-        Environment env = new Environment();
-        Predicate predicate = new Predicate();
-
-        // Prueba con dos átomos iguales
-        assertEquals("true", predicate.execute("(equal 5 5)", env));
-
-        // Prueba con dos átomos diferentes
-        assertEquals("false", predicate.execute("(equal 5 10)", env));
+    public void testAtomWithVariable() {
+        assertEquals("true", predicate.execute("(atom num1)", environment));
+        assertEquals("true", predicate.execute("(atom str1)", environment));
     }
 
     @Test
-    public void testLessThan() {
-        Environment env = new Environment();
-        Predicate predicate = new Predicate();
-
-        // Prueba con dos números donde el primero es menor que el segundo
-        assertEquals("true", predicate.execute("(< 3 5)", env));
-
-        // Prueba con dos números donde el primero es mayor que el segundo
-        assertEquals("false", predicate.execute("(< 5 3)", env));
+    public void testListWithVariable() {
+        assertEquals("true", predicate.execute("(list list1)", environment));
+        assertEquals("false", predicate.execute("(list str1)", environment));
     }
 
     @Test
-    public void testGreaterThan() {
-        Environment env = new Environment();
-        Predicate predicate = new Predicate();
+    public void testEqualWithVariables() {
+        environment.defineVariable("num3", "10");
+        assertEquals("true", predicate.execute("(equal num1 num3)", environment));
+        assertEquals("false", predicate.execute("(equal num1 num2)", environment));
+    }
 
-        // Prueba con dos números donde el primero es mayor que el segundo
-        assertEquals("true", predicate.execute("(> 5 3)", env));
+    @Test
+    public void testLessThanWithVariables() {
+        assertEquals("true", predicate.execute("(< num1 num2)", environment));
+        assertEquals("false", predicate.execute("(< num2 num1)", environment));
+    }
 
-        // Prueba con dos números donde el primero es menor que el segundo
-        assertEquals("false", predicate.execute("(> 3 5)", env));
+    @Test
+    public void testGreaterThanWithVariables() {
+        assertEquals("false", predicate.execute("(> num1 num2)", environment));
+        assertEquals("true", predicate.execute("(> num2 num1)", environment));
+    }
+
+    @Test
+    public void testInvalidArguments() {
+        assertEquals("Error: entrada no válida.", predicate.execute("(atom)", environment));
+        assertEquals("Error: Uno de los operandos no es un número válido",
+                predicate.execute("(< str1 num2)", environment));
     }
 }
