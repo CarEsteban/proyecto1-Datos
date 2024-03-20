9e40b8065fa655664affa0e86104f52e81f02dd4
CarEsteban <car23016@uvg.edu.gt>
2024-03-19 23:10:16 -0600

    quote with arithmetics

--- src/Quote.java
+++ src/Quote.java
@@ -2,19 +2,43 @@ public class Quote implements IFunction {
 
     @Override
     public String execute(String input, Environment env) {
-        String[] tokens = input.trim().replaceAll("[()]", "").trim().split("\\s+");
+        
+        String processedInput = input.trim();
+        if (processedInput.startsWith("(") && processedInput.endsWith(")")) {
+            processedInput = processedInput.substring(1, processedInput.length() - 1).trim();
+        }
 
+        String[] tokens = processedInput.split("\\s+");
+        
         if (tokens.length == 2) {
             String quoted = tokens[1];
-
+            
+            
             if(env.variableExists(quoted)){
-                return env.getStringVariable(quoted);
-            }else{
+                String value = env.getVariable(quoted);
+                return value.toString(); 
+            } else {
                 return quoted;
             }
-
         } else {
-            return "undefined";
+            
+            StringBuilder operation = new StringBuilder();
+            for (int i = 1; i < tokens.length ; i++) { 
+                operation.append(tokens[i]).append(" ");
+            }
+            
+            try {
+                
+                return evaluateArithmeticExpression(operation.toString().trim(), env);
+            } catch (Exception e) {
+                return operation.toString().trim(); 
+            }
         }
+    }
+
+    private String evaluateArithmeticExpression(String expression, Environment env) {
+        
+        ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
+        return arithmeticOperations.execute("( " + expression + " )", env);
     }
 }
